61838cb9d1e154482bfabdcec8f8d50643f450af
JaimeGarecaGRC <44900821+JaimeGarecaGRC@users.noreply.github.com>
2020-02-15 10:28:44 -0400

    Mi primer modelo.

---
+++ Sol1/webappUno/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webappUno.Models
+{ 
+    public class Person
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        [Required(ErrorMessage = "El nombre es requerido")]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(100, ErrorMessage = "Maximo 100 caracteres")]
+        public string Name { get; set; }
+        
+        [Display(Name="Carnet de Identidad")]
+        public string Document { get; set; }
+        
+       
+    }
+}
+
